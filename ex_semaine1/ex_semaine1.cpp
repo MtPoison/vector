42984c65c834977e20e0783496bc8c381ee02207
MtPoison <quentinleforestier0@gmail.com>
2023-10-17 19:11:52 +0200

    angle

--- ex_semaine1/ex_semaine1.cpp
+++ ex_semaine1/ex_semaine1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 
 template<typename T, typename U>
 class Vector {
@@ -63,17 +64,74 @@ public:
         return result.magnetude();
     }
 
-    template<typename T, typename U>
-    Vector<T, U> scaleVector(Vector<T, U>& a) {
-        double choix = 0;
-        std::cout << "Combien veux-tu multiplier ton vecteur ?" << std::endl;
-        std::cin >> choix;
+    Vector<T, U> scaleVector(Vector<T, U>& a, double choix) {
 
         T nouveauX = a.x * choix;
         U nouveauY = a.y * choix;
 
         return Vector<T, U>(nouveauX, nouveauY);
     }
+
+    Vector<T, U> perpendiculaireVector(Vector<T, U>& a, char choix) {
+        T newX;
+        U newY;
+
+        if (choix == 'x') {
+            T newX = -a.x;
+            U newY = a.y;
+        }
+        else {
+            T newX = a.x;
+            U newY = -a.y;
+        }
+
+        return Vector<T, U>(newX, newY);
+    }
+    
+
+    Vector<T, U> reflectVector(Vector<T, U>& a, char choix) {
+        T newX;
+        U newY;
+
+        if (choix == 'x') {
+            T newX = a.x;
+            U newY = -a.y;
+        }
+        else {
+            T newX = -a.x;
+            U newY = a.y;
+        }
+
+        return Vector<T, U>(newX, newY);
+    }
+
+    Vector<T, U> dotVector(Vector<T, U>& Left, Vector<T, U>& Right) {
+
+        Vector<T, U> result = Left.x * Right.x + Left.y * Right.x;
+
+        return Vector<T, U>(result);
+    }
+
+    Vector<T, U> lepVector(Vector<T, U>& Left, Vector<T, U>& Right, double t) {
+
+        t = std::max(0.0, std::min(1.0, t));
+
+        Vector<T, U> result = Left + t * (Right - Left);
+        
+        return Vector<T, U>(result);
+
+    }
+
+    Vector<T, U> lepCamptedVector(Vector<T, U>& Left, Vector<T, U>& Right, double t) {
+
+        t = std::clamp(t, 0.0, 1.0);;
+
+        Vector<T, U> result = Left + t * (Right - Left);
+
+        return Vector<T, U>(result);
+
+    }
+
 
 };
 
@@ -103,7 +161,7 @@ int main() {
 
     Vector<int, int> Left(1, 2);
     Vector<int, int> Right(2, 2);
-    Vector<int, int> resultat = vector.scaleVector(Left);
+    Vector<int, int> resultat = vector.scaleVector(Left, 4);
 
     std::cout << "Vecteur 1 : (" << Left.x << ", " << Left.y << ")" << std::endl;
     std::cout << "Vecteur 2 : (" << Right.x << ", " << Right.y << ")" << std::endl;
